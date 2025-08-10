7d71d214b07b963062a42feadcd7507ddc90e664
Arni1234-cireng <arniflorentina@gmail.com>
2025-08-10 11:07:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title("menghitung :blue[volume kerucut] :rocket:")
 r = st.number_input("masukan jari-jari (cm):",0) 
 t = st.number_input("masukan tinggi (cm):",0)
+if st.button("hitung volume", type="primary"):
+  v = math.1/3phi*(r**2)*t
+  st.succes(f'volume kerucut adalah(v):.2f)'
