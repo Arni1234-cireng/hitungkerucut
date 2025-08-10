281e5a26dc78695e1b7ea6f16b6e655f5768a5ad
Arni1234-cireng <arniflorentina@gmail.com>
2025-08-10 11:30:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,5 @@ r = st.number_input("masukan jari-jari (cm):",0)
 t = st.number_input("masukan tinggi (cm):",0)
 if st.button("hitung volume", type="primary"):
   v = math.pi*(r**2)*t
-  st.success(f'volume kerucut adalah (v:.2f')
+  st.success('volume kerucut adalah!', v="✅")
+  
